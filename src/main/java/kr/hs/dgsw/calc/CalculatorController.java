a5540587b6176935db94347ec727ff6a9ad9745f
dohyeon Ahn <ehgus04614@naver.com>
2019-03-12 10:15:23 +0900

    intialize commit

---
+++ src/main/java/kr/hs/dgsw/calc/CalculatorController.java
@@ -0,0 +1,12 @@
+package kr.hs.dgsw.calc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+    @GetMapping
+    public String getResult(){
+        return "Hello world";
+    }
+}
